76d6bcb3638a0619089ef3f824d2d533df1d9bde
Gorka P <hola@gorka.co>
2015-01-19 11:22:56 +0100

    improved tests

--- sl1.rb
+++ sl1.rb
@@ -8,91 +8,47 @@
 # After writing your class, write several tests. For each one, print “OK” if it succeeded, “FAIL” otherwise, in order to test that
 # everything is working out properly.
 class Calculator
-	def initialize(x, y)
-		@x = x
-		@y = y
+	def addition(x, y)
+		x + y if x.is_a?(Numeric) && y.is_a?(Numeric)
 	end
 
-	def addition
-		@x + @y
+	def substraction(x, y)
+		x - y
 	end
 
-	def substraction
-		@x - @y
+	def multiplication(x, y)
+		x * y
 	end
 
-	def multiplication
-		@x * @y
-	end
-
-	def division
-		@x / @y
+	def division(x, y)
+		x / y
 	end
 end
 
 # TESTING IMPLEMENTING
-first_variable = 1
-second_variable = 2
-calculator = Calculator.new(first_variable, second_variable)
+calculator = Calculator.new
 
 print "Testing addition method: "
-if calculator.addition == 3
+if calculator.addition(1, 2) == 3
 	puts "OK"
 else
 	puts "FAIL"
 end
 
 print "Testing addition method: "
-if calculator.addition == -3
+if calculator.addition([], 2) == 3
 	puts "OK"
 else
 	puts "FAIL"
 end
 
-# -----------------------------------------------------------
+# --- substraction
 
 print "Testing substraction method: "
-if calculator.substraction == -1
+if calculator.substraction(5, 2) == 3
 	puts "OK"
 else
 	puts "FAIL"
 end
 
-print "Testing substraction method: "
-if calculator.substraction == 8
-	puts "OK"
-else
-	puts "FAIL"
-end
-
-# -----------------------------------------------------------
-
-print "Testing multiplication multiplication: "
-if calculator.multiplication == 2
-	puts "OK"
-else
-	puts "FAIL"
-end
-
-print "Testing multiplication multiplication: "
-if calculator.multiplication == 27
-	puts "OK"
-else
-	puts "FAIL"
-end
-
-# -----------------------------------------------------------
-
-print "Testing division division: "
-if calculator.division == 0.0
-	puts "OK"
-else
-	puts "FAIL"
-end
-
-print "Testing division division: "
-if calculator.division == 8.7
-	puts "OK"
-else
-	puts "FAIL"
-end
+# ---
